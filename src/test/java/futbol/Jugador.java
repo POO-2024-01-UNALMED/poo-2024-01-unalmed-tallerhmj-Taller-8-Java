ac282121b10fe5424d9792ca1835e6b5151cdf87
Jaime Luis Osorio Gómez <jaosoriogo@unal.edu.co>
2024-08-21 13:15:45 -0500

    Commit 1 - taller 8
    Se crean las clases respectivas y sus requerimientos

---
+++ src/test/java/futbol/Jugador.java
@@ -0,0 +1,71 @@
+package futbol;
+
+public class Jugador extends Futbolista{
+	
+	public short golesMarcados;
+	public byte dorsal;
+	
+	public Jugador (short golesMarcados, byte dorsal, String nombre, int edad, String posicion) {
+		
+		super(nombre, edad, posicion);
+		this.golesMarcados = golesMarcados;
+		this.dorsal = dorsal;
+			
+	}
+	
+	public Jugador () {
+		
+		super();
+		this.golesMarcados = 289;
+		this.dorsal = 7;
+					
+	}
+	
+	@Override
+	public int compareTo(Futbolista f) {
+		
+		return Math.abs(this.getEdad()- f.getEdad());
+		
+	}
+	
+	@Override
+	public String toString() {
+		
+		return "El futbolista " + getNombre()+ " tiene " + getEdad() + ", y juega de " + getPosicion() + " con el dorsal " + dorsal + ". Ha marcado " 
+		+ golesMarcados;
+		
+	}
+	
+	@Override
+	public boolean jugarConLasManos() {
+		
+		
+		return false;
+		
+	}
+	
+	public void setGolesMarcados(short golesMarcados) {
+		
+		this.golesMarcados = golesMarcados;
+		
+	}
+	
+	public short getGolesMarcados() {
+		
+		return golesMarcados;
+		
+	}
+	
+	public void setDorsal(byte dorsal) {
+		
+		this.dorsal = dorsal;
+		
+	}
+	
+	public byte GetDorsal() {
+		
+		return dorsal;
+		
+	}
+	
+}
